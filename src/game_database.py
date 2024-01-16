cb8fc8769936a8cda7746541aae438ec4c117405
Konoha Tomono-Duval <konohatomonoduval@gmail.com>
2024-01-16 12:24:28 -0700

    Made some progress on logger and game_database

--- src/game_database.py
+++ src/game_database.py
@@ -1,2 +1,10 @@
+import chess
+
 class game_database:
-    pass
+    def __init__(self):
+        pass
+    
+    def display_game(self):
+        pass
+    def display_tournament(self):
+        pass
--- src/logger.py
+++ src/logger.py
@@ -23,7 +23,7 @@ class logger:
         pass
 
     #Internal Work
-    def get_stockfish_results(self, prev_state: str, current_state: str, depth: int = 5) -> float: #Should be refactored to only need one UCI current state
+    def get_stockfish_results(self, prev_state: str, current_state: str, depth: int = 5) -> float: #Takes current and previous FEN states. Can be refactored to only need one UCI current state
         #returns the stockfish analysis of the last move as a positive float
         #Example URL: https://stockfish.online/api/stockfish.php?fen=r2q1rk1/ppp2ppp/3bbn2/3p4/8/1B1P4/PPP2PPP/RNB1QRK1 w - - 5 11&depth=5&mode=eval
         current_FEN = "?fen=" + current_state
@@ -32,19 +32,37 @@ class logger:
         endpoint = "https://stockfish.online/api/stockfish.php"
         current_extra = current_FEN + "&depth=" + str(depth) + "&mode=eval"
         prev_extra = prev_FEN + "&depth=" + str(depth) + "&mode=eval"
-        current_response = str(requests.get(endpoint + current_extra))
-        prev_response = str(requests.get(endpoint + prev_extra))
+        current_response = requests.get(endpoint + current_extra)
+        prev_response = requests.get(endpoint + prev_extra)
 
-        current_score = float(re.findall(r"-?\d*\.*\d+", current_response)[0]) #Positive means white is winning and vice versa
-        prev_score = float(re.findall(r"-?\d*\.*\d+", prev_response)[0])
+        current_string = str(current_response.json())
+        prev_string = str(prev_response.json())
+
+        current_score = float(re.findall(r"-?\d*\.*\d+", current_string)[0]) #Positive means white is winning and vice versa
+        prev_score = float(re.findall(r"-?\d*\.*\d+", prev_string)[0])
         
         
-        return abs(current_score) - abs(prev_score)
+        return abs(current_score) - abs(prev_score) #Positive numbers mean the player that made the move is better off
     def format_game(self):
         pass
 
     #Interface with game_database
-    def return_formatted_game(self, game_num: int):
+    def return_formatted_game(self):
         pass
+        """
+        if self.winner == "":
+            raise RuntimeError("Game is not yet completed")
+            pass
 
+        else:
+            game = {"UCI": self.current_moves, "Cheat Attempts": self.cheat_attempts}
+            return game
+        """
+    
+#Testing section
+if __name__ == "__main__":
+    new_logger = logger("ChessGPT", "ChatGPT")
+    current_board = "r2q1rk1/1pp2ppp/3bbn2/p2p4/8/1B1P4/PPP2PPP/RNB1QRK1 w - - 5 11" #make sure to include the additional notation at the end
+    prev_board = "r2q1rk1/ppp2ppp/3bbn2/3p4/8/1B1P4/PPP2PPP/RNB1QRK1 w - - 5 11"
+    print(str(new_logger.get_stockfish_results(prev_board, current_board)))
     
