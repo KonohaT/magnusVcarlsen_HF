306773cd67f6a6f9073b67e5a5a96af0deac5184
Silas Blanchard <Silas-Blanchard@users.noreply.huggingface.co>
2024-01-17 21:32:18 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ x = st.slider('Select a value')
 st.write(x, 'squared is', x * x)
 
 from transformers import pipeline
-generator2 = pipeline('text-generation', model='meta-llama/Llama-2-7b')
+generator2 = pipeline('text-generation', model='gpt2')
 
 prompt = "Return an opening move in chess."
 generated_text = generator2(prompt, max_length=100, num_return_sequences=1)[0]['generated_text']
