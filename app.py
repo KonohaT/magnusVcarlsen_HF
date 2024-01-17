079849f86e8b2dc158aa7159ae94ef1b91a0cc07
Silas Blanchard <Silas-Blanchard@users.noreply.huggingface.co>
2024-01-17 21:34:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 import streamlit as st
 
-x = st.slider('Select a value')
-st.write(x, 'squared is', x * x)
-
 from transformers import pipeline
 generator2 = pipeline('text-generation', model='gpt2')
 
