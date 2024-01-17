d42283bff384bb646211c6ae6878cebf5eceb902
Silas Blanchard <Silas-Blanchard@users.noreply.huggingface.co>
2024-01-17 21:34:13 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ generator2 = pipeline('text-generation', model='gpt2')
 
 prompt = "Return an opening move in chess."
 generated_text = generator2(prompt, max_length=100, num_return_sequences=1)[0]['generated_text']
-print(generated_text)
+st.write(generated_text)
