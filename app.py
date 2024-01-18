3162c75831f4ec5cd83c2043f95045b91f7de188
Konoha Tomono-Duval <Inkcap@users.noreply.huggingface.co>
2024-01-18 19:36:56 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ generator2 = pipeline('text-generation', model='gpt2')
 
 prompt = "Return an opening move in chess."
 generated_text = generator2(prompt, max_length=100, num_return_sequences=1)[0]['generated_text']
-st.write(generated_text)
+st.write("Tis a string %s", %generated_text)
