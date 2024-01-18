3425e94bd4804a08278c6b006142cb412495ed71
Konoha Tomono-Duval <konohatomonoduval@gmail.com>
2024-01-18 12:43:06 -0700

    Renamed model_runner, changed print to st.write()

--- src/interface_experiment.py
+++ src/interface_experiment.py
@@ -4,5 +4,10 @@ model_id = "BlueSunflower/gpt2-medium-chess"
 text_generator = transformers.pipeline("text-generation", model = model_id)
 input_text = "1-0 2995 3110 1.e4 e5 2.Nf3 Nc6"
 
-generated = text_generator(input_text, max_new_tokens = 500)[0]["generated_text"]
-print(generated)
+for i in range(5):
+    generated = text_generator(input_text, max_new_tokens = 10)[0]["generated_text"]
+    new_string = str(generated).replace(input_text, "")
+    move = new_string.split(" ", maxsplit=1)[1]
+    input_text += move
+    print(input_text)
+
